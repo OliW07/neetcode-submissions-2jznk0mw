bcaed19025c49723992ded9c6e3842288536fb58
Oliver White <80798746+OliW07@users.noreply.github.com>
2026-04-26 10:44:29 +1200

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+       vector<vector<int>> res;
+       res.push_back({});
+
+       for(int num : nums){
+            int size = res.size();
+            for(int i = 0; i < size; i++){
+                vector<int> sub = res[i];
+                sub.push_back(num);
+                res.push_back(sub);
+            }
+       }
+
+       return res;
+    }
+};
